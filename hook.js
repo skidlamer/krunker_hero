ea6e3eaa525be1275243274ce589ec947383f088
skidLamer <skidLamer@mail.com>
2019-09-09 23:04:25 +1000

    Make the redirect a bit easier on the eye and correct oscillator

--- hook.js
+++ hook.js
@@ -1,5 +1,20 @@
-URLS = ["https://krunker.io/js/game*", "https://krunker.io/libs/zip-ext.*", "https://krunker.io/libs/zip.*"];
-chrome.webRequest.onBeforeRequest.addListener(function(details) { return {redirectUrl:chrome.extension.getURL("game.js")}; }, { urls: [URLS[0]] }, ["blocking"]);
-chrome.webRequest.onBeforeRequest.addListener(function(details) { return {redirectUrl:chrome.extension.getURL("zip-ext.js")}; }, { urls: [URLS[1]] }, ["blocking"]);
-chrome.webRequest.onBeforeRequest.addListener(function(details) { return {redirectUrl:chrome.extension.getURL("zip.js")}; }, { urls: [URLS[2]] }, ["blocking"]);
-chrome.webRequest.onHeadersReceived.addListener(function(details) { return {cancel: details.type == "script"} }, { urls: ["https://krunker.io/*"] }, ["blocking"]);
+const items = new Map();
+items.set('game.js', 'https://krunker.io/js/game*');
+items.set('oscillator.js', 'https://krunker.io/libs/oscillator*');
+items.set('zip-ext.js', 'https://krunker.io/libs/zip-ext.*');
+items.set("zip.js", 'https://krunker.io/libs/zip.*');
+
+for (const [file, pattern] of items) {
+    chrome.webRequest.onBeforeRequest.addListener(
+        (requestDetails) => {
+            console.log("Redirecting: " + requestDetails.url);
+            return {
+                redirectUrl: chrome.extension.getURL(file)
+            };
+        }, {
+            urls: [pattern],
+            types: ["script"]
+        },
+        ["blocking"]
+    );
+}
